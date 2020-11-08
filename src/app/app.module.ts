fdfb4528f87eb3fe31498c29d46dcc8790d680db
basb7 <brianalbertosuarez@gmail.com>
2020-11-07 22:34:17 -0500

    load font login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,11 +10,16 @@ import { BlogComponent } from './Components/blog/blog.component';
 import { HomeComponent } from './Components/home/home.component';
 import { MenuComponent } from './Components/menu/menu.component';
 import { from } from 'rxjs';
+import { LoginComponent } from './Components/login/login.component';
+import { SingUpComponent } from './Components/sing-up/sing-up.component';
 
 
 const routesApp: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'Blog', component: BlogComponent }
+  { path: 'Blog', component: BlogComponent },
+  { path: 'Login', component: LoginComponent },
+  { path: 'Singup', component: SingUpComponent }
+
 ]
 
 @NgModule({
@@ -24,6 +29,8 @@ const routesApp: Routes = [
     BlogComponent,
     HomeComponent,
     MenuComponent,
+    LoginComponent,
+    SingUpComponent,
   ],
   imports: [
     BrowserModule,
